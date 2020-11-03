85453d5a3e6bf79e51a8467d68c9833378df5ad8
A00275686 <a00275686@student.ait.ie>
2020-11-03 11:32:39 +0000

    menu added

---
+++ The End of Covid/Assets/Daniel/Code/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //This gets the current scene and goes to the next available scene
+        Time.timeScale = 1f;
+    }
+
+    public void Quit()
+    {
+        UnityEngine.Debug.Log("Quit"); //To see if the quit worked
+        Application.Quit(); //This closes the game
+    }
+}
---
+++ The End of Covid/Assets/Daniel/Code/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GamePaused = false;
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; // resumes the game
+        GamePaused = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; //pauses the game
+        GamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Main Menu"); //loads the main menu
+    }
+}
