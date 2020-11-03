91c7fc4521f277458a7593727ca2144c89d84883
A00269988 <a00269988@student.ait.ie>
2020-11-03 09:17:29 +0000

    Everything

---
+++ The End of Covid/Assets/Caylum/game/Assets/Code/Bacteria.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Bacteria : MonoBehaviour
+{
+    public Rigidbody2D bacteria;
+    public float speed = 3.0f;
+    public GameObject[] gameObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnBacteria", 2);
+    }
+    void SpawnBacteria()
+    {
+        Rigidbody2D bacteriaInstance;
+        bacteriaInstance = Instantiate(bacteria, new Vector3(UnityEngine.Random.Range(-5, 5), 0, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+        bacteriaInstance.name = "Bacteria(Clone)";
+        bacteriaInstance.velocity = new Vector2(speed, 0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
