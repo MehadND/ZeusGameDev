941db09d7200bcfb4efda9e392010486b465a291
MehadND <34424878+MehadND@users.noreply.github.com>
2020-11-08 20:44:43 +0000

    Mehad Changes 2

---
+++ The End of Covid/Assets/VictoryMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryMenu : MonoBehaviour
+{
+    public GUIStyle myStyle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnGUI()
+    {
+        myStyle.normal.textColor = Color.white;
+        myStyle.fontSize = 50;
+        //GUI.Box(new Rect(10, 10, 100, 30), "Time: " + (int)Time.timeSinceLevelLoad, myStyle);
+        GUI.Box(new Rect(500, 232, 100, 100), " " + Player.score, myStyle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
